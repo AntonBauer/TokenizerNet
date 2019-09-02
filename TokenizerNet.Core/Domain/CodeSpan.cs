c1ada69813aeb47a6f3cbae1c55e87d9f61dd8e1
BauerAV <abton.bauer@gmail.com>
2019-09-02 14:30:35 +0200

    Fixed span.Contains error.

--- TokenizerNet.Core/Domain/CodeSpan.cs
+++ TokenizerNet.Core/Domain/CodeSpan.cs
@@ -8,15 +8,12 @@
 
         public CodeSpan(int start, int end)
         {
-            // ToDo use int instead of byte[]
-            //var test = 0xff66;
-
             Start = start;
             End = end;
         }
 
         public CodeSpan(int singlePosition) : this(singlePosition, singlePosition) { }
 
-        public bool Contains(CodeSpan span) => Start <= span.Start && End <= span.End;
+        public bool Contains(CodeSpan span) => Start <= span.Start && span.End <= End;
     }
 }
--- TokenizerNet.Tests/SpanTests.cs
+++ TokenizerNet.Tests/SpanTests.cs
@@ -16,17 +16,15 @@ namespace TokenizerNet.Tests
             var third = new CodeSpan(1488, 1498);
             var fourth = new CodeSpan(64285);
 
-            //fb1d
-
             // Act
             var result_1 = first.Contains(second);
             var result_2 = third.Contains(second);
             var result_3 = fourth.Contains(second);
 
             // Assert
-            Assert.IsTrue(result_1);
-            Assert.IsFalse(result_2);
-            Assert.IsFalse(result_3);
+            Assert.IsTrue(result_1, "84 is inside 41 and 90 failed");
+            Assert.IsFalse(result_2, "84 is not inside 1488 and 1498 failed");
+            Assert.IsFalse(result_3, "84 is not inside 64285 failed");
         }
     }
 }
